bb1456dcdfe72d5d58146a7f95d1fa43b76d9509
Adrianus Kleemans <a.kleemans@gmail.com>
2023-12-05 20:52:30 +0100

    Clean up binaries after timing

--- run-codon.sh
+++
@@ -1,4 +0,0 @@
-# Test for compiling code using codon
-codon run -release day03.py
-codon build -release day03.py
-time ./day03
--- run_codon.py
+++ run_codon.py
@@ -2,6 +2,11 @@ import subprocess
 import datetime
 import time
 
+# # Test for compiling code using codon
+# codon run -release day03.py
+# codon build -release day03.py
+# time ./day03
+
 if __name__ == '__main__':
     today = datetime.datetime.now()
     for d in range(1, today.day + 1):
@@ -10,3 +15,4 @@ if __name__ == '__main__':
         start_time = time.time()
         subprocess.run([f'./{p}'])
         print(p, ':', round(time.time() - start_time, 3), 's')
+        subprocess.run(['rm', f'./{p}'])
