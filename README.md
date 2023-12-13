eb2a4721aee28a1794c620119959020f09497c5d
Adrianus Kleemans <a.kleemans@gmail.com>
2023-12-13 08:45:50 +0100

    Day 12 part 2

--- README.md
+++ README.md
@@ -20,9 +20,33 @@ Timed with Codon 0.16.3: `python3 run_codon.py`
 | Day 9  | 0.004s  |
 | Day 10 | 0.005s  |
 | Day 11 | 0.063s  |
-| Day 12 |         |
+| Day 12 | 0.258s* |
+| Day 13 |         |
+| Day 14 |         |
+| Day 15 |         |
+| Day 16 |         |
+| Day 17 |         |
+| Day 18 |         |
 
 * = Not compiled to Codon yet
 
 ## Learned along the way
+
+### Memoize using `func` (Day 12)
+
+For Dynamic Programming problems, it can be useful to memoize function results.
+Especially for recursive functions, it means that a whole branch can be skipped, as we can look up the result (if we
+calculated it already before).
+
+```python
+from functools import cache
+
+
+@cache
+def place_next(state: str, groups) -> int:
+    ...
+```
+
+Note: `list`s can not be used as function parameters, but they can be converted to `tuple()`.
+
 
--- day12.py
+++ day12.py
@@ -1,4 +1,5 @@
-from typing import List
+from functools import cache
+from typing import List, Tuple
 
 # Day 12: Hot Springs
 
@@ -10,12 +11,14 @@ test_data = '''???.### 1,1,3
 ?###???????? 3,2,1'''.split('\n')
 
 
-def place_next(state: str, groups: List[int], idx: int) -> int:
-    # Try to place
-    to_place = groups.pop(0)
-    if all(c in ['?', '#'] for c in state[idx:(idx + to_place)]) and (
-            idx + to_place == len(state) or state[idx + to_place] != '#'):
-        new_idx = idx + to_place + 1
+@cache
+def place_next(state: str, groups: Tuple) -> int:
+    # Try to place group
+    to_place = groups[0]
+    groups = groups[1:]
+    if all(c in ['?', '#'] for c in state[:to_place]) and (
+            to_place == len(state) or state[to_place] != '#'):
+        new_idx = to_place + 1
 
         # If all groups placed and no more # left, count solution
         if len(groups) == 0:
@@ -24,8 +27,8 @@ def place_next(state: str, groups: List[int], idx: int) -> int:
         else:
             count_sum = 0
             min_rem = sum(groups) + len(groups) - 1
-            for j in range(new_idx, len(state)- min_rem + 1):
-                count_sum += place_next(state, [*groups], j)
+            for j in range(new_idx, len(state) - min_rem + 1):
+                count_sum += place_next(state[j:], groups)
                 # Can't move past a given spring
                 if state[j] == '#':
                     break
@@ -36,12 +39,11 @@ def place_next(state: str, groups: List[int], idx: int) -> int:
 def solve(data: List[str]):
     total_sum = 0
     for line_nr in range(len(data)):
-        print('Working on line', line_nr)
         initial_state, group_str = data[line_nr].split(' ')
         all_groups = [int(n) for n in group_str.split(',')]
         min_remaining = sum(all_groups) + len(all_groups) - 1
-        for i in range(0, len(initial_state)- min_remaining + 1):
-            total_sum += place_next(initial_state, [*all_groups], i)
+        for i in range(0, len(initial_state) - min_remaining + 1):
+            total_sum += place_next(initial_state[i:], tuple(all_groups))
             if initial_state[i] == '#':
                 break
     return total_sum
@@ -66,20 +68,16 @@ def main():
     part1_test_first_line = part1(['?###???????? 3,2,1'])
     assert part1_test_first_line == 10, f'Part 1 test input, first line returned {part1_test_first_line}'
     part1_test_result = part1(test_data)
-    print('Part 1 test input:', part1_test_result)
     assert part1_test_result == 21, f'Part 1 test input returned {part1_test_result}'
     another_test = part1(['.##.?#??.#.?# 2,1,1,1'])
     assert another_test == 1, f'Part 1, another test returned {another_test}'
     part1_result = part1(data)
-    print('Part 1:', part1_result)
     assert part1_result == 7204, f'Part 1 returned {part1_result}'
 
     part2_test_result = part2(test_data)
-    print('Part 2 test input:', part2_test_result)
     assert part2_test_result == 525152, f'Part 2 test input returned {part2_test_result}'
     part2_result = part2(data)
-    print('Part 2:', part2_result)  # remove
-    assert part2_result == 0, f'Part 2 returned {part2_result}'
+    assert part2_result == 1672318386674, f'Part 2 returned {part2_result}'
 
 
 if __name__ == '__main__':
