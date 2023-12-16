8c6d8aea450174526033a57b7f75c1e8f0b56acd
Adrianus Kleemans <a.kleemans@gmail.com>
2023-12-16 16:04:27 +0100

    Update run_codon.py

--- run_codon.py
+++ run_codon.py
@@ -10,11 +10,17 @@ import time
 if __name__ == '__main__':
     times = 20
     today = datetime.datetime.now()
-    for d in range(7, today.day + 1):
+    for d in range(1, today.day + 1):
         p = 'day' + str(d).zfill(2)
+        if d in [8, 12]:
+            # Exclusions not working with Codon:
+            # - Day 8: math.lcm(*args)
+            # - @cache decorator (functools)
+            print(p, ': -')
+            continue
         subprocess.run(['/home/adrianus/.codon/bin/codon', 'build', '-release', f'{p}.py'])
         start_time = time.time()
         for _ in range(times):
             subprocess.run([f'./{p}'])
-        print(p, ':', round((time.time() - start_time) / times, 3), 's')
+        print(p, ':', round((time.time() - start_time) * 1000 / times, 1), 'ms')
         subprocess.run(['rm', f'./{p}'])
