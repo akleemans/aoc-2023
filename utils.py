f5990c96b08afa743de7436e54363927f22dfd7b
Adrianus Kleemans <a.kleemans@gmail.com>
2023-12-27 08:25:37 +0100

    Add utils

---
+++ utils.py
@@ -0,0 +1,61 @@
+from typing import Tuple, List
+
+# Graph / node related stuff
+
+dir_map = {'R': (0, 1), 'L': (0, -1), 'U': (-1, 0), 'D': (1, 0)}
+turn_map = {'R': 'UD', 'L': 'UD', 'U': 'LR', 'D': 'LR'}
+
+
+def in_bounds(row, col, grid) -> bool:
+    """Check if coordinates are in bounds of grid"""
+    return 0 <= row < len(grid) and 0 <= col < len(grid[0])
+
+
+def add(a, b) -> Tuple[int, int]:
+    """Add two coordinate tuples"""
+    return a[0] + b[0], a[1] + b[1]
+
+
+def subtract(a, b) -> Tuple[int, int]:
+    """Subtract two coordinate tuples"""
+    return a[0] - b[0], a[1] - b[1]
+
+
+# PriorityQueue
+
+
+class PriorityQueue:
+    queue: List[Tuple[int, Tuple[int, int], str, int]]
+
+    def __init__(self):
+        self.queue = []
+
+    def empty(self):
+        return len(self.queue) == 0
+
+    def ceil(self, n: float):
+        return int(-1 * n // 1 * -1)
+
+    def floor(self, n):
+        return int(n // 1)
+
+    def put(self, el):
+        """Insert using binary search"""
+        queue = self.queue
+        if len(queue) == 0:
+            queue.append(el)
+            return
+        l = 0
+        r = len(queue) - 1
+        while l != r:
+            m = self.ceil((l + r) / 2)
+            if queue[m] > el:
+                r = m - 1
+            else:
+                l = m
+        if queue[l] < el:
+            l += 1
+        queue.insert(l, el)
+
+    def get(self):
+        return self.queue.pop(0)
---
+++ utils_test.py
@@ -0,0 +1,45 @@
+import unittest
+
+import utils
+
+
+class TestUtils(unittest.TestCase):
+
+    def test_dir_map(self):
+        self.assertEqual((0, 1), utils.dir_map['R'])
+        self.assertEqual((0, -1), utils.dir_map['L'])
+        self.assertEqual((1, 0), utils.dir_map['D'])
+        self.assertEqual((-1, 0), utils.dir_map['U'])
+
+    def test_turn_map(self):
+        self.assertTrue('U' in utils.turn_map['R'])
+        self.assertTrue('D' in utils.turn_map['R'])
+        self.assertTrue('L' in utils.turn_map['D'])
+        self.assertTrue('R' in utils.turn_map['D'])
+
+    def test_in_bounds(self):
+        grid = [[1, 1, 1], [2, 2, 2]]
+        self.assertTrue(utils.in_bounds(0, 0, grid))
+        self.assertTrue(utils.in_bounds(0, 1, grid))
+        self.assertTrue(utils.in_bounds(1, 2, grid))
+        self.assertFalse(utils.in_bounds(2, 1, grid))
+        self.assertFalse(utils.in_bounds(-1, 1, grid))
+
+    def test_add(self):
+        self.assertEqual((3, 5), utils.add((1, 2), (2, 3)))
+        self.assertEqual((4, 4), utils.add((0, 1), (4, 3)))
+
+    def test_subtract(self):
+        self.assertEqual((-1, -1), utils.subtract((1, 2), (2, 3)))
+        self.assertEqual((0, 1), utils.subtract((4, 1), (4, 0)))
+
+    def test_priority_queue(self):
+        queue = utils.PriorityQueue()
+        queue.put(4)
+        queue.put(2)
+        queue.put(1)
+        queue.put(3)
+        queue.put(5)
+        self.assertEqual([1, 2, 3, 4, 5], queue.queue)
+        self.assertEqual(1, queue.get())
+        self.assertEqual([2, 3, 4, 5], queue.queue)
