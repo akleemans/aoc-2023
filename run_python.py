148c303b2ed23a5b5c80690b0cf4866a736eb681
Adrianus Kleemans <a.kleemans@gmail.com>
2023-12-28 16:01:51 +0100

    Work on python timing script

--- run_python.py
+++ run_python.py
@@ -1,13 +1,12 @@
 import subprocess
-import datetime
 import time
 
 if __name__ == '__main__':
     times = 20
-    today = datetime.datetime.now()
-    for d in range(8, today.day + 1):
+    # today = datetime.datetime.now()
+    for d in range(1, 26):
         p = 'day' + str(d).zfill(2)
-        if d in [5]:
+        if d in [5, 22, 23]:
             print(p, ': -')
             continue
         start_time = time.time()
